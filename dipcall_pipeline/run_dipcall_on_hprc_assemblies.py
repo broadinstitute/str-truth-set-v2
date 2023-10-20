046ef66565a465f99347a807d4dbb8920df8aacc
bw2 <ben.weisburd@gmail.com>
2023-10-19 23:58:21 -0400

    fixed dipcall command for male samples

--- dipcall_pipeline/run_dipcall_on_hprc_assemblies.py
+++ dipcall_pipeline/run_dipcall_on_hprc_assemblies.py
@@ -12,6 +12,7 @@ Increased mutation and gene conversion within human segmental duplications by Vo
   https://www.nature.com/articles/s41586-023-05895-y
 
 """
+
 import os
 import pandas as pd
 from step_pipeline import pipeline, Backend, Localize
@@ -25,7 +26,7 @@ parser = bp.get_config_arg_parser()
 parser.add_argument("-s", "--sample-id", action="append",
                     help="Process only this sample. Can be specified more than once.")
 parser.add_argument("--more-memory", action="store_true", help="Run with 2x more memory")
-parser.add_argument("--output-dir", default="gs://str-truth-set-v2/hprc_dipcall")
+parser.add_argument("--output-dir", default="gs://str-truth-set-v2/dipcall_pipeline")
 args = bp.parse_known_args()
 
 df = pd.read_table("hprc_assemblies.tsv")
@@ -37,6 +38,7 @@ df["url_mat"] = df["accession_mat"].map(accession_to_url_map)
 if args.sample_id:
     df = df[df.sample_id.isin(args.sample_id)]
 
+s1_steps = []
 for i, (_, row) in enumerate(df.iterrows()):
 
     s1 = bp.new_step(
@@ -48,6 +50,8 @@ for i, (_, row) in enumerate(df.iterrows()):
         storage="50Gi",
         output_dir=os.path.join(args.output_dir, row.sample_id))
 
+    s1_steps.append(s1)
+
     hg38_fasta_input, _ = s1.inputs(
         "gs://str-truth-set/hg38/ref/hg38.fa",
         "gs://str-truth-set/hg38/ref/hg38.fa.fai",
@@ -56,7 +60,10 @@ for i, (_, row) in enumerate(df.iterrows()):
     s1.command("set -exuo pipefail")
     s1.command(f"cd /io")
     s1.command(f"wget --quiet {row.url_pat} & wget --quiet {row.url_mat} & wait")
+
+    pseudoautosomal_region_arg = "-x /dipcall.kit/hs38.PAR.bed" if row["sex"] == "male" else ""
     s1.command(f"/dipcall.kit/run-dipcall "
+               f"{pseudoautosomal_region_arg} "
                f"{row.sample_id} "
                f"{hg38_fasta_input} "
                f"{os.path.basename(row.url_pat)} "
@@ -86,10 +93,12 @@ s2 = bp.new_step(
     cpu=1,
     output_dir=args.output_dir)
 
+s2.depends_on(s1_steps)
+
 s2.command("set -exuo pipefail")
 for _, row in df.iterrows():
-    current_input = s2.input(os.path.join(args.output_dir, f"{row.sample_id}.dip.bed"))
-    s2.command(f"cat {current_input} >> all.dip.bed")
+    current_input = s2.input(os.path.join(args.output_dir, row.sample_id, f"{row.sample_id}.dip.bed.gz"))
+    s2.command(f"zcat {current_input} >> all.dip.bed")
 output_bed_filename = f"combined.high_confidence_regions.{len(df)}_samples.union.bed.gz"
 s2.command(f"bedtools sort -i all.dip.bed | bedtools merge -i - | bgzip > {output_bed_filename}")
 s2.command(f"tabix {output_bed_filename}")
