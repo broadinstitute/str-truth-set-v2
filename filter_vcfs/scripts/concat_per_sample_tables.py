10173045f0a4748fd4294d2205c5cf5b6ee87e37
bw2 <ben.weisburd@gmail.com>
2023-10-26 23:31:35 -0400

    added optional -c arg

--- filter_vcfs/scripts/concat_per_sample_tables.py
+++ filter_vcfs/scripts/concat_per_sample_tables.py
@@ -4,25 +4,34 @@ import argparse
 import os
 import pandas as pd
 
-parser = argparse.ArgumentParser(description='Concatenate per-sample tables')
-parser.add_argument('-o', '--output-tsv', help='Combined tsv file path')
-parser.add_argument("input_tsvs", nargs='+', help="Input tsv files")
+parser = argparse.ArgumentParser(description="Concatenate per-sample tables")
+parser.add_argument("-o", "--output-tsv", help="Combined tsv file path")
+parser.add_argument("-c", "--columns", action="append", help="Columns to keep")
+parser.add_argument("input_tsvs", nargs="+", help="Input tsv files")
 args = parser.parse_args()
 
 if not args.output_tsv:
     args.output_tsv = f"concatenated.{len(args.input_tsvs)}_tables.tsv.gz"
 
-if not args.output_tsv.endswith('.gz'):
+if not args.output_tsv.endswith(".gz"):
     args.output_tsv += ".gz"
+
+if args.columns and "LocusId" not in args.columns:
+    args.columns = ["LocusId"] + args.columns
+    print("Will keep columns: " + ", ".join(args.columns))
 
 combined_df = None
 for i, input_tsv in enumerate(args.input_tsvs):
-    sample_id = os.path.basename(input_tsv).split('.')[0]
+    sample_id = os.path.basename(input_tsv).split(".")[0]
 
     df = pd.read_table(input_tsv)
     print(f"Read {sample_id:10s} table with {len(set(df.LocusId)):10,d} unique loci")
 
+    if args.columns:
+        df = df[args.columns]
+
     df["SampleId"] = sample_id
+
     if combined_df is None:
         combined_df = df
     else:
@@ -31,4 +40,4 @@ for i, input_tsv in enumerate(args.input_tsvs):
 print(f"Writing combined table with {len(combined_df):,d} rows and {len(set(combined_df.LocusId)):,d} unique loci to",
       args.output_tsv)
 
-combined_df.to_csv(args.output_tsv, sep='\t', index=False)
+combined_df.to_csv(args.output_tsv, sep="\t", index=False)
