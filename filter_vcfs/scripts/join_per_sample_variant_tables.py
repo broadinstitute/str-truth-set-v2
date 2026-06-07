e4c3f691b8f7736e881c478a743f385ae9d288df
bw2 <ben.weisburd@gmail.com>
2026-06-07 15:39:32 -0400

    Fix argparse action typo and bump submodule to code review fixes

--- filter_vcfs/scripts/join_per_sample_variant_tables.py
+++ filter_vcfs/scripts/join_per_sample_variant_tables.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(description="Perform an outer-join on multiple 
 parser.add_argument("-o", "--output-tsv", help="Combined tsv file path")
 parser.add_argument("-n", type=int, help="Number of tables to process")
 parser.add_argument("--discard-impure-genotypes", action="store_true", help="Discard genotypes that are not pure repeats")
-parser.add_argument("--output-stats-tsv", action="store_strue", help="If specified, will output a table with stats.")
+parser.add_argument("--output-stats-tsv", action="store_true", help="If specified, will output a table with stats.")
 parser.add_argument("input_tsvs", nargs="+", help="Input tsv files")
 args = parser.parse_args()
 
